4c673df2fded1d433506f48ea51842d0b14a2044
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2020-01-06 17:22:25 +0100

    added documentation

--- callbacks.js
+++ callbacks.js
@@ -1,29 +1,30 @@
+//Example 1
 function a(callback){
-    // Simulamos un tiempo de espera en la ejecución de la función a
+  // We simulate the waiting time for executing function A
     setTimeout( function(){
-      console.log("Se ha ejecutado la función a");
+      console.log("Function A has been executed");
       callback();
     }, 1000 );
     
 }
   
   function b(){
-    console.log("Se ha ejecutado la función b");
+    console.log("Function b has been executed");
   }
   
   a(b);
 
 
-//CALLBACK EXAMPLE
-//   Example 1
 
-  function getUserName(callback){
-    var name;
-    $.get('https://randomuser.me/api/',  function(data) {
-            name = data.results[0].name.first
-                    + " " + data.results[0].name.last;
-            callback(name);
-    });
+//Example 2
+
+function getUserName(callback){
+  var name;
+  $.get('https://randomuser.me/api/',  function(data) {
+          name = data.results[0].name.first
+                  + " " + data.results[0].name.last;
+          callback(name);
+  });
 }
 var username ;
 function callback(res){
@@ -33,49 +34,49 @@ function callback(res){
 getUserName(callback);
 
 
-    // Example 2
-    var seconds = 20;
-    function startCountDown(){
-      setInterval(function(){
-        seconds--;
-        showSeconds();
-      }, 1000);
-    }
-    function showSeconds(){
-       console.log(seconds);
-    }
-    startCountDown();
+// Example 3
+  var seconds = 20;
+  function startCountDown(){
+    setInterval(function(){
+      seconds--;
+      showSeconds();
+    }, 1000);
+  }
+  function showSeconds(){
+      console.log(seconds);
+  }
+  startCountDown();
 
-    // Example 3
-    function doHomework(subject, callback) {
-        alert(`Starting my ${subject} homework.`);
-        callback();
-      }
-      
-      doHomework('math', function() {
-        alert('Finished my homework');
-      });
-
-    // Example 4 JQUERY VERSION
-    $(document).ready(function(){
-    $("button").click(function(){
-        $("h4").toggle("slow", function(){
-        console.log("Action with callback");
-        });
-    });
-    });
-    //Example 4 JavaScript Example
-    document.querySelector("button").onclick = function(){
-        document.querySelector("h4").classList.toggle("hide");
-            console.log("Actionw with callback");
-    }
-
-    //Example 5 
-    function hagoAlgo(callback) {
-        callback('Hola Anexsoft !!');
+// Example 4
+  function doHomework(subject, callback) {
+      alert(`Starting my ${subject} homework.`);
+      callback();
     }
     
-    hagoAlgo(function(valorQueMeSetearon){
-        console.log(valorQueMeSetearon);
-    })
+    doHomework('math', function() {
+      alert('Finished my homework');
+    });
+
+  // Example 4 with Jquery
+  $(document).ready(function(){
+  $("button").click(function(){
+      $("h4").toggle("slow", function(){
+      console.log("Action with callback");
+      });
+  });
+  });
+  //Example 4 
+  document.querySelector("button").onclick = function(){
+      document.querySelector("h4").classList.toggle("hide");
+          console.log("Callback Action");
+  }
+
+  //Example 5 
+  function sayHi(callback) {
+      callback('Hello!');
+  }
+  
+  sayHi(function(settedValue){
+      console.log(settedValue);
+  })
 
